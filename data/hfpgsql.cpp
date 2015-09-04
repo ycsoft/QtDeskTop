fa7ef0354b7c7518bd0ebad161725461f33be7f3
XD.Yang <xd.yang@nf-3.com>
2015-09-04 23:23:58 +0800

    add hfpgsql library code

---
+++ data/hfpgsql.cpp
@@ -0,0 +1,74 @@
+﻿#include "hfpgsql.h"
+#include <sstream>
+
+HFPGSql::HFPGSql():_conn(NULL),_result(NULL)
+{
+    _configure.connected = false;
+}
+
+HFPGSql::~HFPGSql()
+{
+
+    if ( NULL != _result )
+        PQclear(_result);
+    if ( NULL != _conn )
+        PQfinish(_conn);
+}
+
+bool HFPGSql::connectDB(PGConfigure *config)
+{
+    ostringstream ostr;
+    _configure = (*config);
+    //memcpy(&_configure,config,sizeof(PGConfigure));
+    ostr<<"host="<<config->host.c_str()<<" port="<<config->port
+        <<" dbname="<<config->dbname.c_str()<<" user="<<config->usr
+          <<" password="<<config->pwd;
+    string connstr = ostr.str();
+
+    _conn = PQconnectdb(connstr.c_str());
+
+    if ( CONNECTION_BAD != PQstatus(_conn) )
+    {
+        _configure.connected = true;
+
+        return true;
+    }else
+    {
+        _configure.connected = false;
+        return false;
+    }
+
+}
+
+PGresult * HFPGSql::querySQL(string sql)
+{
+    if ( !_configure.connected )
+        return NULL;
+    PGresult *res = PQexec(_conn,sql.c_str());
+
+    _result = res;
+    return res;
+}
+
+int HFPGSql::getRecordCount()
+{
+    return PQntuples(_result);
+}
+
+int HFPGSql::getColumnCount()
+{
+    return PQnfields(_result);
+}
+
+string HFPGSql::fieldName(int column)
+{
+    int columns = PQnfields(_result);
+    if ( column >= columns )
+        return "";
+    return string(PQfname(_result,column));
+}
+
+string HFPGSql::fieldValue(int row, int col)
+{
+    return PQgetvalue(_result,row,col);
+}
---
+++ data/hfpgsql.h
@@ -0,0 +1,50 @@
+﻿#ifndef HFPGSQL_H
+#define HFPGSQL_H
+
+
+#include <libpq-fe.h>
+#include <string>
+
+using namespace std;
+
+struct PGConfigure
+{
+    void operator = ( const PGConfigure &conf)
+    {
+        host = conf.host;
+        dbname = conf.dbname;
+        usr = conf.usr;
+        pwd = conf.pwd;
+        port = conf.port;
+        connected = conf.connected;
+    }
+
+    string host;
+    string dbname;
+    string usr;
+    string pwd;
+    int    port;
+    bool   connected;
+};
+
+class HFPGSql
+{
+public:
+    HFPGSql();
+    ~HFPGSql();
+
+    bool        connectDB(PGConfigure *config);
+
+    PGresult    *querySQL(string sql);
+    int         getRecordCount();
+    int         getColumnCount();
+    string      fieldName( int column );
+    string      fieldValue( int row, int col);
+
+private:
+    PGConfigure     _configure;
+    PGresult        *_result;
+    PGconn          *_conn;
+};
+
+#endif // HFPGSQL_H
