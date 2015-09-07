e1730f04c056b922b7d365f9d3de8adf64f0fd25
XD.Yang <xd.yang@nf-3.com>
2015-09-07 22:28:21 +0800

    begin to design some ui with html5/css/js

---
+++ html/res/test.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+//改变对象的背景颜色
+function mouseIn( obj )
+{
+//	var elem = document.getElementById("all");
+	obj.style.backgroundColor = "#666666"; 
+}
+function mouseOut(obj)
+{
+	obj.style.backgroundColor = "#2F2F2F"; 
+}
+function listList( obj )
+{
+	
+}
