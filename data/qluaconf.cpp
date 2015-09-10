fe9ce6cff938ee849210e5df0fcd72d0e29880ad
XD.Yang <xd.yang@nf-3.com>
2015-09-10 21:06:06 +0800

    read config file

--- data/qluaconf.cpp
+++ data/qluaconf.cpp
@@ -1,7 +1,55 @@
-#include "qluaconf.h"
+﻿#include "qluaconf.h"
 
 QLuaConf::QLuaConf(QObject *parent) : QObject(parent)
 {
-
+    int res;
+    m_lua = luaL_newstate();
+    luaL_openlibs(m_lua);
+    //luaL_loadfile(m_lua,"lua/appset.lua");
+    res = luaL_dofile(m_lua,"lua/appset.lua");
+    if ( res != 0 )
+    {
+        qDebug()<<"error";
+    }
 }
 
+QLuaConf::~QLuaConf()
+{
+    lua_close(m_lua);
+}
+
+QString QLuaConf::getString(QString key)
+{
+    lua_getglobal(m_lua,key.toStdString().c_str());
+    QString res = luaL_checkstring(m_lua,-1);
+    lua_pop(m_lua,1);
+    return res;
+}
+int QLuaConf::getInteger(QString key)
+{
+    lua_getglobal(m_lua,key.toStdString().c_str());
+    int res = luaL_checkinteger(m_lua,-1);
+    return res;
+}
+QList<QLuaConf::srvconf> QLuaConf::getSrvList(QString key)
+{
+    QList<QLuaConf::srvconf> res;
+    lua_getglobal(m_lua,key.toStdString().c_str());
+    if ( lua_istable(m_lua,-1))
+    {
+        qDebug()<<"Is Table";
+    }
+    int it = lua_gettop(m_lua);
+    lua_pushnil(m_lua);
+    it  = 1;
+    while( 0!= lua_next(m_lua,it))
+    {
+        QLuaConf::srvconf conf;
+        conf.key = QString::fromLocal8Bit(luaL_checkstring(m_lua,-2));
+        conf.value = QString::fromLocal8Bit(luaL_checkstring(m_lua,-1));
+        res.push_back(conf);
+        lua_pop(m_lua,1);
+    }
+    lua_pop(m_lua,1);
+    return res;
+}
--- data/qluaconf.h
+++ data/qluaconf.h
@@ -1,17 +1,49 @@
-#ifndef QLUACONF_H
+﻿#ifndef QLUACONF_H
 #define QLUACONF_H
 
 #include <QObject>
+#include <lua.hpp>
+#include <QDebug>
+#include <QList>
 
+/**
+ * @brief The QLuaConf class
+ * 从LUA配置文件中读取应用程序的配置
+ */
 class QLuaConf : public QObject
 {
     Q_OBJECT
 public:
+
+    struct   srvconf
+    {
+        QString key;
+        QString value;
+    };
+
     explicit QLuaConf(QObject *parent = 0);
+    ~QLuaConf();
+    /**
+     * @brief getString
+     * 获取字符串数据项
+     * @param key：关键字 key = value
+     * @return 字符串
+     */
+    QString  getString(QString key);
+    /**
+     * @brief getInteger
+     * 获取整数数据项
+     * @param key
+     * @return
+     */
+    int      getInteger(QString key);
+    QList<srvconf>   getSrvList(QString key);
 
 signals:
 
 public slots:
+private:
+    lua_State   *m_lua;
 };
 
 #endif // QLUACONF_H
