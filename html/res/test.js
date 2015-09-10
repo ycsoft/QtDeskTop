382f961f482bdd563427974304c8566c576df5f2
XD.Yang <xd.yang@nf-3.com>
2015-09-10 21:06:49 +0800

    login window and support CA

--- html/res/test.js
+++ html/res/test.js
@@ -21,10 +21,10 @@ function clickTask(txt)
 }
 
 //添加一件事项
-function addTask()
+function addTask(txt)
 {
 	
-	var test = "<h3>项目支出明细</h3><p>单位：国库处</p><p>审核：张三</p><p>摘要:2014年度总共支付543万元，其中....</p>";
+	var test = txt;//"<h3>项目支出明细</h3><p>单位：国库处</p><p>审核：张三</p><p>摘要:2014年度总共支付543万元，其中....</p>";
 	if ( curCol < colMax && curRow > 0)
 	{
 		if( curRow  >= 1)
@@ -82,4 +82,20 @@ function runQt()
 {
 	var s = QSimpleMode.invoke();
 	alert(s);
+}
+
+function  cfunc_get(sql)
+{
+	var res = QSimpleMode.get(sql);
+	return res;
+}
+
+function showMenu()
+{
+	var pop = window.createPopup();
+	//alert(pop);
+	  pop.document.oncontextmenu=function(){
+                      return false;};
+	  pop.show(event.clientX-1,event.clientY,200,2,document.body);
+				
 }
