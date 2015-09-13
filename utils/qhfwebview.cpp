b8582a5a940a4146a00a0c4746b0cf818ec63604
XD.Yang <xd.yang@nf-3.com>
2015-09-13 23:00:00 +0800

    add hf webview control

--- utils/qhfwebview.cpp
+++ utils/qhfwebview.cpp
@@ -1,7 +1,32 @@
-#include "qhfwebview.h"
+﻿#include "qhfwebview.h"
 
-QHFWebView::QHFWebView(QWidget *parent) : QWidget(parent)
+#include <QPainter>
+#include <QStyle>
+#include <QStyleOption>
+#include <QPalette>
+
+QHFWebView::QHFWebView(QWidget *parent) : QWebView(parent)
 {
 
 }
 
+
+//void QHFWebView::paintEvent(QPaintEvent *evt)
+//{
+//    QPainter p;
+//    QStyleOption op;
+
+//    //QWebView::paintEvent(evt);
+
+//    op.init(this);
+//    style()->drawPrimitive(QStyle::PE_Widget,&op,&p,this);
+//}
+
+void QHFWebView::setTransparent(bool )
+{
+    QPalette palette = this->palette();
+    palette.setBrush(QPalette::Base,Qt::transparent);
+
+    this->page()->setPalette(palette);
+    setAttribute(Qt::WA_OpaquePaintEvent,false);
+}
--- utils/qhfwebview.h
+++ utils/qhfwebview.h
@@ -1,14 +1,19 @@
-#ifndef QHFWEBVIEW_H
+﻿#ifndef QHFWEBVIEW_H
 #define QHFWEBVIEW_H
 
 #include <QWidget>
+#include <QWebView>
 
-class QHFWebView : public QWidget
+class QHFWebView : public QWebView
 {
     Q_OBJECT
 public:
     explicit QHFWebView(QWidget *parent = 0);
 
+    void     setTransparent( bool );
+
+protected:
+//    void paintEvent(QPaintEvent *);
 signals:
 
 public slots:
