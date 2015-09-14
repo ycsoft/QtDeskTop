7ec8bce7ac1233f1b4d8e9ccc5faf45403f1d87d
XD.Yang <xd.yang@nf-3.com>
2015-09-14 22:54:21 +0800

    software center

--- softCenter/qsoftcenter.cpp
+++ softCenter/qsoftcenter.cpp
@@ -1,7 +1,17 @@
-#include "qsoftcenter.h"
+﻿#include "qsoftcenter.h"
+
+#include <QHBoxLayout>
 
 QSoftCenter::QSoftCenter(QWidget *parent) : QWidget(parent)
 {
-
+    initUI();
 }
 
+void QSoftCenter::initUI()
+{
+    QHBoxLayout *lay = new QHBoxLayout(this);
+    m_web = new QHFWebView(this);
+    m_web->load(QUrl("html/softCenter.html"));
+    lay->addWidget(m_web);
+    resize(600,400);
+}
--- softCenter/qsoftcenter.h
+++ softCenter/qsoftcenter.h
@@ -1,17 +1,27 @@
-#ifndef QSOFTCENTER_H
+﻿#ifndef QSOFTCENTER_H
 #define QSOFTCENTER_H
 
-#include <QWidget>
+#include "utils/qhfwebview.h"
 
+#include <QWidget>
+/**
+ * @brief The QSoftCenter class
+ * 软件中心功能界面
+ */
 class QSoftCenter : public QWidget
 {
     Q_OBJECT
 public:
     explicit QSoftCenter(QWidget *parent = 0);
 
+protected:
+    void    initUI();
 signals:
 
 public slots:
+
+private:
+    QHFWebView      *m_web;
 };
 
 #endif // QSOFTCENTER_H
