df70cb2e8460a394167bb0997575417a93689638
XD.Yang <xd.yang@nf-3.com>
2015-09-16 15:22:07 +0800

    add jscore in the qwebview core

--- utils/qhfwebview.cpp
+++ utils/qhfwebview.cpp
@@ -1,9 +1,13 @@
 ﻿#include "qhfwebview.h"
+#include "jsCore/qjscore.h"
 
 #include <QPainter>
 #include <QStyle>
 #include <QStyleOption>
 #include <QPalette>
+#include <QMouseEvent>
+#include <QWebSettings>
+#include <QWebFrame>
 
 QHFWebView::QHFWebView(QWidget *parent) : QWebView(parent)
 {
@@ -22,6 +26,11 @@ QHFWebView::QHFWebView(QWidget *parent) : QWebView(parent)
 //    style()->drawPrimitive(QStyle::PE_Widget,&op,&p,this);
 //}
 
+void QHFWebView::mouseDoubleClickEvent(QMouseEvent *evt)
+{
+    evt->ignore();
+}
+
 void QHFWebView::setTransparent(bool )
 {
     QPalette palette = this->palette();
@@ -29,4 +38,19 @@ void QHFWebView::setTransparent(bool )
 
     this->page()->setPalette(palette);
     setAttribute(Qt::WA_OpaquePaintEvent,false);
+}
+
+void QHFWebView::registerObject()
+{
+    QJSCore *core = new QJSCore(this);
+    page()->mainFrame()->addToJavaScriptWindowObject("Qt",core);
+}
+
+void QHFWebView::load(const QUrl &url)
+{
+    QWebView::load(url);
+    QWebSettings *sets = this->page()->settings();
+    sets->setAttribute(QWebSettings::JavascriptEnabled,true);
+    sets->setAttribute(QWebSettings::LocalContentCanAccessFileUrls,true);
+    connect(page()->mainFrame(),SIGNAL(javaScriptWindowObjectCleared()),this,SLOT(registerObject()));
 }
--- utils/qhfwebview.h
+++ utils/qhfwebview.h
@@ -11,12 +11,15 @@ public:
     explicit QHFWebView(QWidget *parent = 0);
 
     void     setTransparent( bool );
+    void     load(const QUrl& url);
 
 protected:
 //    void paintEvent(QPaintEvent *);
+    void mouseDoubleClickEvent(QMouseEvent *evt);
 signals:
 
 public slots:
+    void    registerObject();
 };
 
 #endif // QHFWEBVIEW_H
