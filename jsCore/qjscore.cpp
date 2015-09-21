11f2d0c558e5b24d29e55dbdfddcf47996c4d150
XD.Yang <xd.yang@nf-3.com>
2015-09-22 00:48:28 +0800

    为 JS提供访问postgresql数据库的能力

--- jsCore/qjscore.cpp
+++ jsCore/qjscore.cpp
@@ -7,14 +7,21 @@
 #include <QMessageBox>
 #include <QDebug>
 #include <QProcess>
+#include <string>
+using namespace std;
 
 
-
-QJSCore::QJSCore(QObject *parent) : QObject(parent)
+QJSCore::QJSCore(QObject *parent) : QObject(parent),m_pgsql(NULL)
 {
 
 }
-
+QJSCore::~QJSCore()
+{
+    if( m_pgsql )
+    {
+        delete m_pgsql;
+    }
+}
 
 QString QJSCore::readFromFile(const QString &fn)
 {
@@ -89,4 +96,40 @@ void QJSCore::showWindow(QString win)
         idx = ( idx == Simple_Desk ? Full_Desk : Simple_Desk);
         pstack->setCurrentIndex(idx);
     }
+}
+
+int QJSCore::connectDB(QString host, QString dbname, QString usr, QString pwd)
+{
+    PGConfigure  conf;
+    conf.host = host.toStdString();
+    conf.dbname = dbname.toStdString();
+    conf.usr = usr.toStdString();
+    conf.pwd = usr.toStdString();
+    m_pgsql = new HFPGSql();
+    if ( m_pgsql->connectDB(&conf) )
+    {
+        return 0;
+    }else
+    {
+        return -1;
+    }
+}
+
+void QJSCore::executeSQL(QString sql)
+{
+    m_pgsql->querySQL(sql.toStdString());
+}
+
+int QJSCore::getRecordCount()
+{
+    return m_pgsql->getRecordCount();
+}
+int QJSCore::getColumnCount()
+{
+    return m_pgsql->getColumnCount();
+}
+QString QJSCore::fieldValue(int row, int col)
+{
+    QString res = QString::fromUtf8( m_pgsql->fieldValue(row,col).c_str() );
+    return res;
 }
--- jsCore/qjscore.h
+++ jsCore/qjscore.h
@@ -1,6 +1,8 @@
 ﻿#ifndef QJSCORE_H
 #define QJSCORE_H
 
+#include "data/hfpgsql.h"
+
 #include <QObject>
 #include <QWidget>
 /**
@@ -14,6 +16,8 @@ class QJSCore : public QObject
 public:
     explicit QJSCore(QObject *parent = 0);
 
+    ~QJSCore();
+
 signals:
 
 public slots:
@@ -23,6 +27,15 @@ public slots:
     void     open(const QString &path);
     void     open(const QString &path,const QStringList &arglist);
     void     showWindow(QString);
+
+    int      connectDB(QString host, QString dbname,QString usr,QString pwd);
+    void     executeSQL(QString sql);
+
+    int         getRecordCount();
+    int         getColumnCount();
+    QString      fieldValue( int row, int col);
+private:
+    HFPGSql         *m_pgsql;
 };
 
 #endif // QJSCORE_H
