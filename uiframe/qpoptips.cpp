c3619e18284181fee894dd295cf17711389b1c15
XD.Yang <xd.yang@nf-3.com>
2015-09-24 00:44:07 +0800

    add poptios

---
+++ uiframe/qpoptips.cpp
@@ -0,0 +1,25 @@
+﻿#include "qpoptips.h"
+#include <QVBoxLayout>
+
+QPopTips::QPopTips(QWidget *parent) : QDialog(parent)
+{
+    initUI();
+}
+
+QPopTips::~QPopTips()
+{
+
+}
+void QPopTips::setTips(const QString &usr, const QString &etti, const QString &data)
+{
+
+}
+
+void QPopTips::initUI()
+{
+    QVBoxLayout *vlay = new QVBoxLayout(this);
+
+    m_web = new QHFWebView(this);
+    vlay->setContentsMargins(0,0,0,0);
+    vlay->addWidget(m_web);
+}
---
+++ uiframe/qpoptips.h
@@ -0,0 +1,25 @@
+﻿#ifndef QPOPTIPS_H
+#define QPOPTIPS_H
+
+#include "utils/qhfwebview.h"
+
+#include <QDialog>
+
+class QPopTips : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit QPopTips(QWidget *parent = 0);
+    void     setTips(const QString &usr, const QString &etti, const QString &data);
+    ~QPopTips();
+protected:
+    void            initUI();
+signals:
+
+public slots:
+
+private:
+    QHFWebView      *m_web;
+};
+
+#endif // QPOPTIPS_H
