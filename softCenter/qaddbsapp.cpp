80bee1342457a0bb46489aa88e19f83114fe6a4c
Seven-Yang <xd.yang@nf-3.com>
2015-11-06 23:15:17 +0800

    add bs add function

--- softCenter/qaddbsapp.cpp
+++ softCenter/qaddbsapp.cpp
@@ -1,7 +1,53 @@
-#include "qaddbsapp.h"
+﻿#include "qaddbsapp.h"
+
+#include "jsCore/qjscore.h"
+#include "maindialog.h"
+#include "uiframe/qappiconpanel.h"
+
+#include <QVBoxLayout>
+#include <QWebFrame>
+#include <QDebug>
 
 QAddBSApp::QAddBSApp(QWidget *parent) : QWidget(parent)
 {
-
+    initUI();
+    resize(500,430);
+    setWindowFlags(Qt::FramelessWindowHint);
+    setVisible(false);
 }
 
+
+void QAddBSApp::initUI()
+{
+    QWidget *center = new QWidget(this);
+    QVBoxLayout *vlay = new QVBoxLayout(center);
+    QVBoxLayout *mainLay = new QVBoxLayout(this);
+
+    vlay->setMargin(0);
+    mainLay->setMargin(0);
+
+    m_web = new QHFWebView(center);
+    vlay->addWidget(m_web);
+
+    mainLay->addWidget(center);
+    m_web->load(QUrl("html/addapp.html"));
+
+    connect(m_web->page()->mainFrame(),SIGNAL(javaScriptWindowObjectCleared()),this,SLOT(addObject()));
+}
+
+void QAddBSApp::addObject()
+{
+    m_web->page()->mainFrame()->addToJavaScriptWindowObject("BS",this);
+}
+
+void QAddBSApp::okClick()
+{
+    QJSCore *jscore = QJSCore::ref();
+    QString bsname = jscore->readValue("bs-name");
+    QString url = jscore->readValue("bs-url");
+
+    QAppIconPanel *panel = MainDialog::ref().getAppIconPanel();
+    panel->addApp(QPixmap("html/images/ie.png"),bsname,url);
+    MainDialog::ref().saveApp(bsname,url);
+    hide();
+}
--- softCenter/qaddbsapp.h
+++ softCenter/qaddbsapp.h
@@ -1,5 +1,7 @@
-#ifndef QADDBSAPP_H
+﻿#ifndef QADDBSAPP_H
 #define QADDBSAPP_H
+
+#include "utils/qhfwebview.h"
 
 #include <QWidget>
 
@@ -8,10 +10,20 @@ class QAddBSApp : public QWidget
     Q_OBJECT
 public:
     explicit QAddBSApp(QWidget *parent = 0);
+    ~QAddBSApp(){}
 
+
+protected:
+    void                initUI();
 signals:
 
 public slots:
+    void                addObject();
+
+    void                okClick();
+private:
+    QHFWebView          *m_web;
+
 };
 
 #endif // QADDBSAPP_H
